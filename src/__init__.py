c24af714190e1bccd922a6bf5a7501fbe8896141
Peppo2002 <giuseppemariapollini72@gmail.com>
2025-04-03 16:44:57 +0200

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from brfs import RisolutoreBRFS 
-from astar import RisolutoreAStar
-from dfs import RisolutoreDFS
-from bfs import RisolutoreBFS  # Assicurati che il nome sia corretto
-
-__all__ = ['RisolutoreBRFS', 'RisolutoreAStar', 'RisolutoreDFS', 'RisolutoreBFS']
